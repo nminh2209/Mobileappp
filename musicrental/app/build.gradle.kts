7b4a6165aa7b41f533dd5be1dd85088d42ef4cef
Nguyen Minh <nminh22092209@gmail.com>
2025-03-14 12:21:42 +0700

    test still failing

--- musicrental/app/build.gradle.kts
+++ musicrental/app/build.gradle.kts
@@ -14,8 +14,10 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+
     }
 
     buildTypes {
@@ -37,12 +39,27 @@ android {
 }
 
 dependencies {
+    // Core dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.junit.ktx)
+    implementation(libs.core)
+
+    // Unit testing
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.androidx.junit)
+    testImplementation(libs.androidx.core)
+
+
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.runner.v152)  // Test runner
+    androidTestImplementation(libs.androidx.rules)
+
 }
+
+
+
